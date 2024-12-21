9005546ca99b77d4707ac7de22d9cdec4b66f173
Ayas <ayas95ebraheem@gmail.com>
2024-12-21 19:49:41 +0200

    Twitter connection

---
+++ build.sbt
@@ -0,0 +1,17 @@
+ThisBuild / version := "0.1.0-SNAPSHOT"
+
+ThisBuild / scalaVersion := "2.13.10"
+
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "BigDataProj",
+    libraryDependencies ++= Seq(
+      "org.apache.kafka" %% "kafka" % "2.8.0",
+      "ch.qos.logback" % "logback-classic" % "1.4.11", // for SLF4J logging
+      "org.slf4j" % "slf4j-nop" % "2.0.9" ,// Suppresses the warning
+      "org.twitter4j" % "twitter4j-core" % "4.0.7",
+      "org.twitter4j" % "twitter4j-stream" % "3.0.3"
+    )
+  )
+
---
+++ src/main/scala/App/ApiController/TwitterKafkaProducer.scala
@@ -0,0 +1,39 @@
+package App.ApiController
+import java.util.Properties
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import twitter4j._
+import twitter4j.conf.ConfigurationBuilder
+
+object TwitterKafkaProducerApp {
+  def main(args: Array[String]): Unit = {
+
+
+
+    // Twitter API credentials
+    val consumerKey = "N3uhWhwUvNQBSdY5JhrcK0Jpg"
+    val consumerSecret = "Gj8FLQjSJJziRkXfCxtXBkF1nuPUYTStz4BMNyGRKC9I4EoEPh"
+    val accessToken = "1869792054798057473-E2ap9sTnQRUX0h1CzW4TUO8gvW6y46"
+    val accessTokenSecret = "nuhsl2pOxCj7IY4ZpymIDAEtFMwlQsmlPczB8E16TgMhW"
+
+
+    // Set up the configuration
+    val cb = new ConfigurationBuilder()
+    cb.setOAuthConsumerKey(consumerKey)
+      .setOAuthConsumerSecret(consumerSecret)
+      .setOAuthAccessToken(accessToken)
+      .setOAuthAccessTokenSecret(accessTokenSecret)
+
+
+    // Initialize Twitter instance
+    val twitter = new TwitterFactory(cb.build()).getInstance()
+
+
+    try {
+      val user = twitter.verifyCredentials()
+      println(s"Successfully connected to Twitter as ${user.getName}")
+    } catch {
+      case e: TwitterException =>
+        println(s"Failed to connect to Twitter: ${e.getMessage}")
+    }
+  }
+}
